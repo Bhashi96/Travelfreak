e167d394c795a785486e1e601b45387f5b16bdd7
erandidilshani <36620007+erandidilshani@users.noreply.github.com>
2020-03-09 17:56:35 +0530

    Update GudregisterController.php

--- app/Http/Controllers/GudregisterController.php
+++ app/Http/Controllers/GudregisterController.php
@@ -7,6 +7,9 @@ use Auth;
 use App\User;
 use App\Gudregisters;
 use Illuminate\Support\Facades\Validator;
+//
+use Illuminate\Support\Facades\Hash; 
+use Illuminate\Support\Facades\Storage; 
 
 class GudregisterController extends Controller
 {
@@ -18,6 +21,7 @@ class GudregisterController extends Controller
     public function index()
     {
         $gudregisters = Gudregisters::all()->toArray();
+        
         return view('serviceprovider.gudhome', compact('gudregisters'));
     }
 
@@ -39,33 +43,33 @@ class GudregisterController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate($request,[
+        // $this->validate($request,[
 
-        'name' => 'required',
-        'email'=> 'required',
-        'contact'=> 'required',
-        'age'=> 'required',
-        'gender'=> 'required',
-        'licence'=> 'required',
-        'area' => 'required',
-        'price' => 'required'
+        // 'name' => 'required',
+        // 'email'=> 'required',
+        // 'contact'=> 'required',
+        // 'age'=> 'required',
+        // 'gender'=> 'required',
+        // 'licence'=> 'required',
+        // 'area' => 'required',
+        // 'price' => 'required'
 
-        ]);
+        // ]);
 
-        $gudregister= new Gudregister([
+        // $gudregister= new Gudregister([
 
-            'name' => $request->get('name'),
-            'email'=> $request->get('email'),
-            'contact'=> $request->get('contact'),
-            'age'=> $request->get('age'),
-            'gender'=> $request->get('gender'),
-            'licence' => $request->get('licence'),
-            'area'=> $request->get('area'),
-            'price'=> $request->get('price')
+        //     'name' => $request->get('name'),
+        //     'email'=> $request->get('email'),
+        //     'contact'=> $request->get('contact'),
+        //     'age'=> $request->get('age'),
+        //     'gender'=> $request->get('gender'),
+        //     'licence' => $request->get('licence'),
+        //     'area'=> $request->get('area'),
+        //     'price'=> $request->get('price')
 
-        ]);
-        $gudregister->save();
-        return redirect()->route('serviceprovider.gudedit')->with('success','Data Added');
+        // ]);
+        // $gudregister->save();
+        // return redirect()->route('serviceprovider.gudedit')->with('success','Data Added');
     }
 
     /**
@@ -99,13 +103,9 @@ class GudregisterController extends Controller
      */public function validate1(array $data){
         return validator::make($data,[
             'name'=>['required','string','max:255'],
-            // 'email'=>['required','string','max:255'],
-            // 'contact'=>['required','string'],   
-            // 'age'=>['required','string'],
-            // 'gender'=>['required','string'],
-             // 'licence'=>['required','string'],
-            //'area' =>['required','string'],
-            //'price'=>['required','int'],
+            'contact'=>['required','string'],   
+            'age'=>['required','string'],
+            'area' =>['required','string'],
 
             // 'name'=>[''],
             // 'email'=>[''],
@@ -114,33 +114,42 @@ class GudregisterController extends Controller
             // 'gender'=>[''],
             // 'licence'=>[''],
              //'area' =>['required','string'],
-            //'price'=>['required','int'],
+            // 'price'=>['required','int'],
 
         ]);
 
     }
 
     public function update(Request $request,$id){
-        // dd($request->name);
+        // dd($request->all());
         $this->validate1($request->all())->validate();
-        $user=user::find($id);
-        $id=Auth::user()->gud->id;
-        // dd($id);
-        $gudregister=$gudregisters::find($id);
-        // dd($gudregister);
+        $gudregister=gudregisters::find($id);
         // dd($gudregister);
 
+        $user=Auth::user();
+        // $user_row=user::find($user);
+        // dd($user_row->name);
+
+        // dd($user->name);
         $user->name = $request->input('name');
 
-        $gudregister->name = $request->input('name');
-        // $gudregister->email = $request->input('email');
-        // $gudregister->contact = $request->input('contact');
-        // $gudregister->age = $request->input('age');
-        // $gudregister->gender = $request->input('gender');
-        // $gudregister->licence = $request->input('licence');
-        // $gudregister->area = $request->input('area' );
-        // $gudregister->price = $request->input('price' );
+        // dd($id);
+        // $gudregister=$gudregistecrs::find($id);
+        // dd($gudregister);
+        // dd($gudregister);
 
+        $gudregister->name = $request->input('name');
+        $gudregister->contact = $request->input('contact');
+        $gudregister->age = $request->input('age');
+        $gudregister->area = $request->input('area' );
+        // $gudregister->price=$request->input('price');
+
+        if( $request-> has('image_path')){
+            $image=$request->file('image_path');
+            $filename = $image->getClientOriginalName();
+            $image->move(public_path('images/guide'),$filename);
+            $gudregister ->image_path=$request->file('image_path')->getClientOriginalName();
+        }
         $gudregister->save();
         $user->save();
 
